69fe4306178e5ff9fcd33bc533afbbd9cf70132f
Minh12s <minhtnth2209037@fpt.edu.vn>
2024-04-03 15:01:15 +0700

    1

---
+++ Model/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace examapi.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Tel { get; set; }
+        public string Department { get; set; }
+    }
+
+}
